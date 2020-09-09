be6d76b7d0783f5c3ec12115fc0a6a940ea8678e
Mvtthew <mateusz2345678@gmail.com>
2020-09-09 15:10:16 +0200

    :tada: Init + React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Home } from './views/home/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Error404 } from './views/error404/Error404';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render = () => {
+		return (
+			<div className="app">
+				<Router>
+					<Switch>
+						<Route path="/" exact>
+							<Home></Home>
+						</Route>
+						<Route path="/">
+							<Error404></Error404>
+						</Route>
+					</Switch>
+				</Router>
+			</div>
+		);
+	};
 }
 
 export default App;
---
+++ src/views/error404/Error404.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class Error404 extends React.Component {
+	// TODO Make better 404 error page
+	render = () => {
+		return <p>404</p>;
+	};
+}
